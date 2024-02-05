a066376fd8fcfb2ea13094fee65786c66997f4a5
Виктор Лобынцев <sibfight@mail.ru>
2024-02-05 22:59:34 +0300

    add gendiff option -f

--- gendiff.js
+++ gendiff.js
@@ -2,9 +2,10 @@
 const { program } = require('commander');
 
 program
-	.name('gendiff')
 	.description('Compares two configuration files and shows a difference.')
 	.version('1.0.0', '-V, --version', 'output the version number')
-	.helpOption('-h, --help', 'display help for command');
-
-program.parse(process.argv);
+	.argument('<filepath1>', 'path to first config file')
+	.argument('<filepath2>', 'path to second config file')
+	.option('-f, --format <type>', 'output format')
+	.helpOption('-h, --help', 'output usage information')
+	.parse(process.argv);
