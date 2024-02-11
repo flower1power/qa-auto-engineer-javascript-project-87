b5d8e00902e6dacf345060859baaf67b05268b38
Виктор Лобынцев <sibfight@mail.ru>
2024-02-11 16:29:03 +0300

    fix lint

--- src/formattes/stylish.js
+++ src/formattes/stylish.js
@@ -2,14 +2,16 @@ import _ from 'lodash';
 
 const getIndent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
 
-
 const stringify = (value, depth) => {
   if (!_.isPlainObject(value)) {
     return `${value}`;
   }
   const result = _.keys(value).map((key) => {
     const childrenValue = value[key];
-    return `${getIndent(depth)}  ${key}: ${stringify(childrenValue, depth + 1)}`;
+    return `${getIndent(depth)}  ${key}: ${stringify(
+      childrenValue,
+      depth + 1,
+    )}`;
   });
 
   return `{\n${result.join('\n')}\n${getIndent(depth - 1)}  }`;
@@ -18,17 +20,36 @@ const stringify = (value, depth) => {
 const stylish = (node, depth = 1) => {
   switch (node.type) {
     case 'nested':
-      return `${getIndent(depth)}  ${node.key}: {\n${node.children.map((child) => stylish(child, depth + 1)).join('\n')}\n${getIndent(depth)}  }`;
+      return `${getIndent(depth)}  ${node.key}: {\n${node.children
+        .map((child) => stylish(child, depth + 1))
+        .join('\n')}\n${getIndent(depth)}  }`;
     case 'added':
-      return `${getIndent(depth)}+ ${node.key}: ${stringify(node.children, depth + 1)}`;
+      return `${getIndent(depth)}+ ${node.key}: ${stringify(
+        node.children,
+        depth + 1,
+      )}`;
     case 'deleted':
-      return `${getIndent(depth)}- ${node.key}: ${stringify(node.children, depth + 1)}`;
+      return `${getIndent(depth)}- ${node.key}: ${stringify(
+        node.children,
+        depth + 1,
+      )}`;
     case 'changed':
-      return `${getIndent(depth)}- ${node.key}: ${stringify(node.children, depth + 1)}\n${getIndent(depth)}+ ${node.key}: ${stringify(node.children2, depth + 1)}`;
+      return `${getIndent(depth)}- ${node.key}: ${stringify(
+        node.children,
+        depth + 1,
+      )}\n${getIndent(depth)}+ ${node.key}: ${stringify(
+        node.children2,
+        depth + 1,
+      )}`;
     case 'unchanged':
-      return `${getIndent(depth)}  ${node.key}: ${stringify(node.children, depth + 1)}`;
+      return `${getIndent(depth)}  ${node.key}: ${stringify(
+        node.children,
+        depth + 1,
+      )}`;
     case 'root':
-      return `{\n${(node.children.map((obj) => stylish(obj, depth))).join('\n')}\n}`;
+      return `{\n${node.children
+        .map((obj) => stylish(obj, depth))
+        .join('\n')}\n}`;
     default:
       throw new Error(`wrong type ${node.type}`);
   }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 import buildTree from './dataCompare.js';
-import parser from './parsers.js'
-import formatter from './formattes/index.js'
+import parser from './parsers.js';
+import formatter from './formattes/index.js';
 
 const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
-	const pathOne = path.resolve(filepath1);
-	const pathTwo = path.resolve(filepath2);
+  const pathOne = path.resolve(filepath1);
+  const pathTwo = path.resolve(filepath2);
 
-	const data1 = readFileSync(pathOne);
-	const data2 = readFileSync(pathTwo);
+  const data1 = readFileSync(pathOne);
+  const data2 = readFileSync(pathTwo);
 
-	const extOne = path.extname(filepath1);
-	const extTwo = path.extname(filepath2);
+  const extOne = path.extname(filepath1);
+  const extTwo = path.extname(filepath2);
 
-	const obj1 = parser(data1, extOne);
-	const obj2 = parser(data2, extTwo);
+  const obj1 = parser(data1, extOne);
+  const obj2 = parser(data2, extTwo);
 
-	const data = buildTree(obj1, obj2);
-	return formatter(data, formatName);
+  const data = buildTree(obj1, obj2);
+  return formatter(data, formatName);
 };
 export default gendiff;
